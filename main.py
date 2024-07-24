cb8f81dfe6004a802e436c007a6dac48e7c5e560
sineom <sineom@126.com>
2024-07-24 22:14:42 +0800

    版本号的添加

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def find_json(json_string):
     return json_string
 
 
-@plugins.register(name="summary", desire_priority=-1, desc="A simple plugin to summary messages", version="0.3.2",
+@plugins.register(name="summary-v2", desire_priority=-1, desc="A simple plugin to summary messages", version="0.0.2",
                   author="lanvent")
 class Summary(Plugin):
     def __init__(self):
